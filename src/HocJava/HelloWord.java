dcd313d476a96541262bf18abce65248ddfe3b34
nguyendangkin <nguyenchin0077@gmail.com>
2025-06-04 15:46:36 +0700

    Variables (Khái niệm biến)

--- src/HocJava/HelloWord.java
+++ src/HocJava/HelloWord.java
@@ -2,10 +2,10 @@ package HocJava;
 
 public class HelloWord {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		System.out.println("Hello world");
-		System.out.println("Hello world");
-		System.out.println("Hello world");
-		System.out.println("Hello world");
+		int hoiDanIt = 5 + 9 * 3 - 10 / 2;
+		int test_10;
+		test_10 = 10;
+
+		System.out.println("dap an la:" + hoiDanIt);
 	}
 }
