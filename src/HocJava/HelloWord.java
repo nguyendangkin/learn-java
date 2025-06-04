e49cc724bfbfd9f40681ff76906ab14397ee8f7f
nguyendangkin <nguyenchin0077@gmail.com>
2025-06-04 21:00:30 +0700

    bài tập lab 1

--- src/HocJava/HelloWord.java
+++ src/HocJava/HelloWord.java
@@ -4,14 +4,16 @@ import java.util.Scanner;
 
 public class HelloWord {
 	public static void main(String[] args) {
-
+		String name;
+		int score;
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.println("firstNumber is: ");
-		int firstNumber = scanner.nextInt();
-		System.out.println("lastNumber is: ");
-		int lastNumber = scanner.nextInt();
-		System.out.println("Min = " + Math.min(firstNumber, lastNumber));
+		System.out.println("Nhập tên của bạn:");
+		name = scanner.nextLine();
+		System.out.println("Nhập điểm của bạn:");
+		score = scanner.nextInt();
+
+		System.out.println(name + " có điểm = " + score);
 
 		scanner.close();
 
