4791f2a1f8ae5ac36f7e3bbb84b14fd8311b0c09
nguyendangkin <nguyenchin0077@gmail.com>
2025-06-04 15:36:02 +0700

    first commit

---
+++ src/HocJava/HelloWord.java
@@ -0,0 +1,11 @@
+package HocJava;
+
+public class HelloWord {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+	}
+}
