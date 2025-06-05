e871804cc411168e8791098beb35213054e54b59
nguyendangkin <nguyenchin0077@gmail.com>
2025-06-05 15:25:48 +0700

    bài 1 phương trình bật nhất oke

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int a;
+        int b;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Vui long nhap so nguyen a:");
+        a = scanner.nextInt();
+        System.out.println("Vui long nhap so nguyen b:");
+        b = scanner.nextInt();
+
+        if (a == 0 && b == 0) {
+            System.out.println("Phuong trinh co vo so nghiem");
+        } else if (a == 0 && b != 0) {
+            System.out.println("Phuong trinh vo nghiem");
+        } else {
+            double x = -(double) b / a;
+            System.out.println("Phuong trinh co nghiem x la: " + x);
+        }
+
+        scanner.close();
     }
 }
