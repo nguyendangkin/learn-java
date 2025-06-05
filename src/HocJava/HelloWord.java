47b130f28d77ec4166b4274aff8eef838ff250e0
nguyendangkin <nguyenchin0077@gmail.com>
2025-06-05 12:41:16 +0700

    swich case

--- src/HocJava/HelloWord.java
+++ src/HocJava/HelloWord.java
@@ -4,15 +4,31 @@ import java.util.Scanner;
 
 public class HelloWord {
 	public static void main(String[] args) {
-		double canh;
 
+		System.out.println(">> LỰA CHỌN TÍNH NĂNG <<");
+		System.out.println("++ ------------------ ++");
+		System.out.println("| 1. Cộng |");
+		System.out.println("| 2. Trừ |");
+		System.out.println("| 3. Kết thúc |");
+		System.out.println("++ ------------------ ++");
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Nhap canh lap phuong:");
-		canh = scanner.nextDouble();
+		System.out.println("Lựa chọn của bạn là : ");
+		int chose = scanner.nextInt();
+		switch (chose) {
+		case 1:
+			System.out.println("Bạn đã lựa chọn phép cộng");
+			break;
+		case 2:
+			System.out.println("Bạn đã lựa chọn phép trừ");
+			break;
+		case 3:
+			System.out.println("Bạn đã lựa chọn kết thúc");
 
-		System.out.println("The tich la: " + Math.pow(3, canh));
-
-		scanner.close();
+			break;
+		default:
+			System.out.println("Bạn không lựa chọn cái gì cả");
+			System.exit(0);
+		}
 
 	}
 }
