c3b8c80d9668561662628319ad0fb4cae5944935
nguyendangkin <nguyenchin0077@gmail.com>
2025-06-05 07:02:54 +0700

    bai tập lap 1 - 3

--- src/HocJava/HelloWord.java
+++ src/HocJava/HelloWord.java
@@ -4,33 +4,13 @@ import java.util.Scanner;
 
 public class HelloWord {
 	public static void main(String[] args) {
-//		String name;
-//		int score;
-//		Scanner scanner = new Scanner(System.in);
-//
-//		System.out.println("Nhập tên của bạn:");
-//		name = scanner.nextLine();
-//		System.out.println("Nhập điểm của bạn:");
-//		score = scanner.nextInt();
-//
-//		System.out.println(name + " có điểm = " + score);
-//
-//		scanner.close();
-		float height;
-		float width;
+		double canh;
+
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Nhập chiều cao:");
-		height = scanner.nextFloat();
-		System.out.println("Nhập chiều rộng:");
-		width = scanner.nextFloat();
-		System.out.println("Chu vi hình chữ nhật là:");
-		System.out.println((height + width) * 2);
+		System.out.println("Nhap canh lap phuong:");
+		canh = scanner.nextDouble();
 
-		System.out.println("Diện tích hình chữ nhật là:");
-		System.out.println(height * width);
-
-		System.out.println("Cạnh nhỏ nhất của hình chữ nhật là:");
-		System.out.println(Math.min(width, height));
+		System.out.println("The tich la: " + Math.pow(3, canh));
 
 		scanner.close();
 
