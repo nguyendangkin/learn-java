dc129440c8fdef87e6e522771b87a9dc1d6530a8
nguyendangkin <nguyenchin0077@gmail.com>
2025-06-06 09:48:07 +0700

    sap xep so lon nhat nho nhat

--- src/App.java
+++ src/App.java
@@ -1,15 +1,30 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập số nguyên: ");
-        int number = scanner.nextInt();
+        System.out.print("Nhap so nguyen: ");
+        int n = scanner.nextInt();
+
+        System.out.printf("Nhap so luong phan tu mang ma ban muon: ");
+        n = scanner.nextInt();
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Phan tu thu " + (i + 1));
+            numbers[i] = scanner.nextInt();
+        }
         scanner.close();
 
-        for (int i = 1; i <= 10; i++) {
-            System.out.printf("%d x %d = %d%n", number, i, number * i);
-        }
+        Arrays.sort(numbers);
+        System.out.println("Mang da xap xep la: " + Arrays.toString(numbers));
+
+        int minValue = numbers[0];
+        int maxValue = numbers[numbers.length - 1];
+
+        System.out.println("gia tri lon nhat: " + maxValue);
+        System.out.println("gia tri nho nhat: " + minValue);
 
     }
 
